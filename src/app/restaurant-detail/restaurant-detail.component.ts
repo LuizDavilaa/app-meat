57815faa7f2afc4ef282f4fceee38d198a4c84e9
LuizDavilaa <luiz.davila@etec.sp.gov.br>
2021-11-09 16:48:33 -0300

    restaurant-detail Dinâmico - Parte 1

--- src/app/restaurant-detail/restaurant-detail.component.ts
+++ src/app/restaurant-detail/restaurant-detail.component.ts
@@ -1,4 +1,7 @@
+import { Restaurant } from './../restaurants/restaurant/restaurant.model';
+import { RestaurantsService } from './../restaurants/restaurants.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-restaurant-detail',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurantDetailComponent implements OnInit {
 
-  constructor() { }
+  restaurants: Restaurant;
+
+  constructor(private restaurantService: RestaurantsService,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.restaurantService.restaurantById(this.route.snapshot.params['id'])
+    .subscribe(restaurants =>{
+      this.restaurants = restaurants;
+    })
   }
 
 }
